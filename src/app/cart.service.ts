8a315ff91ca24604741c8449ad0d47c331206144
AkilJoseph02 <akil.joseph92@student.qcc.cuny.edu>
2022-03-17 17:41:02 -0400

    2 files modified

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Product } from './products';
 /* . . . */
 
@@ -7,6 +8,14 @@ import { Product } from './products';
 })
 export class CartService {
   items: Product[] = [];
+
+  constructor(
+    private http: HttpClient
+  ) {}
+
+  getShippingPrices() {
+    return this.http.get<{type: string, price: number}[]>('/assets/shipping.json');
+  }
   /* . . . */
   addToCart(product: Product) {
     this.items.push(product);
@@ -20,7 +29,7 @@ export class CartService {
     this.items = [];
     return this.items;
   }
-  
+
 }
 
 
